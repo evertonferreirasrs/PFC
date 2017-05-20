1d10cff4c7d5acdcec6fdca5378457cf2ffbb1dc
Marcos Antonio <marcaosi2014@gmail.com>
2017-05-20 16:23:24 -0300

    Arquivo SQL para criacao do banco de dados atualizado com o novo modelo.

--- Model/sql/DML - LOCALIZAÊ.sql
+++ Model/sql/DML - LOCALIZAÊ.sql
@@ -3,8 +3,8 @@
 	email VARCHAR(255) NOT NULL,
 	nome VARCHAR(255) NOT NULL,
 	senha VARCHAR(32) NOT NULL,
-	bloqueado BOOLEAN NOT NULL DEFAULT false,
-	motivo TEXT,
+	situacao VARCHAR(50) NOT NULL DEFAULT false,
+	motivo TEXT NULL,
 	tokenRedeSocial VARCHAR(32),
 	tokenAutenticacao VARCHAR(32),
 	dataHoraExpiracaoToken TIMESTAMP,
@@ -21,25 +21,19 @@ CREATE TABLE tipoUsuario(
 CREATE TABLE criterioAvaliacao(
 	id BIGSERIAL NOT NULL,
 	nome VARCHAR(255) NOT NULL,
+        peso bigint NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE estande(
 	id BIGSERIAL NOT NULL,
 	curso VARCHAR(255) NOT NULL,
-	descricao TEXT NOT NULL,
+	descricao TEXT NULL,
 	periodo BIGINT NOT NULL,
-	nome VARCHAR(255) NOT NULL,
+	titulo VARCHAR(255) NOT NULL,
 	areaTematica VARCHAR(255) NOT NULL,
 	numero BIGINT NOT NULL,
-	PRIMARY KEY(id)
-);
-
-CREATE TABLE informacoesParaAvaliacao(
-	id BIGSERIAL NOT NULL,
-	criterioAvaliacao_fk BIGINT NOT NULL,
-	usuario_fk BIGINT NOT NULL,
-	estande_fk BIGINT NOT NULL,
+        evento_fk BIGINT NOT NULL,
 	PRIMARY KEY(id)
 );
 
@@ -47,7 +41,11 @@ CREATE TABLE avaliacaoJurado(
 	id BIGSERIAL NOT NULL,
 	nota BIGINT NOT NULL,
 	opiniao TEXT NOT NULL,
-	informacoesParaAvaliacao_fk BIGINT NOT NULL UNIQUE,
+	status VARCHAR(50) NOT NULL,
+        dataHoraFechamento TIMESTAMP NULL,
+        dataHoraAbertura TIMESTAMP NULL,
+        usuario_fk BIGINT NOT NULL,
+        criterioAvaliacao_fk BIGINT NOT NULL,
 	PRIMARY KEY(id)
 );
 
@@ -61,17 +59,17 @@ CREATE TABLE avaliacaoVisitante(
 );
 
 CREATE TABLE integranteEquipe(
-	id BIGSERIAL NOT NULL,
 	usuario_fk BIGINT NOT NULL,
 	estande_fk BIGINT NOT NULL,
 	responsavel BOOLEAN DEFAULT FALSE,
-	PRIMARY KEY(id)
+	PRIMARY KEY(usuario_fk, estande_fk)
 );
 
 CREATE TABLE imagem(
 	id BIGSERIAL NOT NULL,
 	uri VARCHAR(255) NOT NULL,
 	estande_fk BIGINT,
+        promocao_fk BIGINT,
 	PRIMARY KEY(id)
 );
 
@@ -81,18 +79,30 @@ CREATE TABLE promocao(
 	descricao TEXT NOT NULL,
 	dataHora TIMESTAMP NOT NULL,
 	estande_fk BIGINT NOT NULL,
-	imagem_fk BIGINT,
 	PRIMARY KEY(id)
 );
 
+CREATE TABLE evento(
+    id BIGSERIAL NOT NULL,
+    nome VARCHAR(255) NOT NULL,
+    dataHoraEventoInicio TIMESTAMP NOT NULL,
+    dataHoraEventoFim TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE criterioJurado(
+    usuario_fk BIGINT NOT NULL,
+    criterioAvaliacao_fk BIGINT NOT NULL,
+    PRIMARY KEY(usuario_fk, criterioAvaliacao_fk)
+);
+
 ALTER TABLE usuario ADD CONSTRAINT usuario_tipoUsuario_fk FOREIGN KEY (tipoUsuario_fk) REFERENCES tipoUsuario(id) ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE informacoesParaAvaliacao ADD CONSTRAINT info_usuario_fk FOREIGN KEY (usuario_fk) REFERENCES usuario(id) ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE informacoesParaAvaliacao ADD CONSTRAINT info_estande_fk FOREIGN KEY (estande_fk) REFERENCES estande(id) ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE avaliacaoJurado ADD CONSTRAINT avaliacao_informacoesParaAvaliacao_fk FOREIGN KEY (informacoesParaAvaliacao_fk) REFERENCES informacoesParaAvaliacao(id) ON UPDATE CASCADE ON DELETE NO ACTION;
 ALTER TABLE avaliacaoVisitante ADD CONSTRAINT avaliacaoVisitante_usuario_fk FOREIGN KEY (usuario_fk) REFERENCES usuario(id) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE avaliacaoVisitante ADD CONSTRAINT avaliacaoVisitante_estande_fk FOREIGN KEY (estande_fk) REFERENCES estande(id) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE integranteEquipe ADD CONSTRAINT integranteEquipe_usuario_fk FOREIGN KEY (usuario_fk) REFERENCES usuario(id) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE integranteEquipe ADD CONSTRAINT integranteEquipe_estande_fk FOREIGN KEY (estande_fk) REFERENCES estande(id) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE imagem ADD CONSTRAINT imagem_estande_fk FOREIGN KEY (estande_fk) REFERENCES estande(id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE imagem ADD CONSTRAINT imagem_promocao_fk FOREIGN KEY (promocao_fk) REFERENCES promocao(id) ON UPDATE CASCADE ON DELETE CASCADE;
 ALTER TABLE promocao ADD CONSTRAINT promocao_estande_fk FOREIGN KEY (estande_fk) REFERENCES estande(id) ON UPDATE CASCADE ON DELETE CASCADE;
-ALTER TABLE promocao ADD CONSTRAINT promocao_imagem_fk FOREIGN KEY (imagem_fk) REFERENCES imagem(id) ON UPDATE CASCADE ON DELETE NO ACTION;
+ALTER TABLE criterioJurado ADD CONSTRAINT criterioJurado_usuario_fk FOREIGN KEY (usuario_fk) REFERENCES usuario(id) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE criterioJurado ADD CONSTRAINT criterioJurado_criterioAvaliacao_fk FOREIGN KEY (criterioAvaliacao_fk) REFERENCES criterioAvaliacao(id) ON UPDATE CASCADE ON DELETE CASCADE;
