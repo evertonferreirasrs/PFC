5d54d7ef3ace4fd06aa64bdf65eca68f747bde39
Marcos Antonio dos Santos <marcaosi2014@gmail.com>
2017-09-15 01:52:03 -0300

    add cors

--- WebService/src/java/br/net/localizae/webservice/controller/UsuarioController.java
+++ WebService/src/java/br/net/localizae/webservice/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UsuarioController {
+    
+    @CrossOrigin(origins = "http://localhost:8080")
     @RequestMapping(value = "usuario", method = RequestMethod.GET)
     public ResponseEntity readByCriteria(String nome, String email, String senha, Long tipoUsuario, String situacao, Long limit, Long offset){
         List<Usuario> usuarioList = null;
