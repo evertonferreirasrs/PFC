64bb471d34f508f3536c0b14d3fccdadcdf154e6
Marcos Antonio dos Santos <marcaosi2014@gmail.com>
2017-10-11 20:04:48 -0300

    Mensagem de carregando implementada na tela que mostra as possibilidades de avaliacao do jurado

--- mobile/LocalizaE/app/src/main/java/localizae/net/br/controller/Fragments/AvaliarEstandeFragment.java
+++ mobile/LocalizaE/app/src/main/java/localizae/net/br/controller/Fragments/AvaliarEstandeFragment.java
@@ -1,8 +1,11 @@
 package localizae.net.br.controller.Fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +26,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import localizae.net.br.controller.Activity.LoginActivity;
 import localizae.net.br.controller.R;
 import localizae.net.br.dao.EstandeDAO;
 import localizae.net.br.model.CriterioAvaliacao;
@@ -92,15 +96,54 @@ public class AvaliarEstandeFragment extends Fragment {
 
         usuario.setCriterioAvaliacaoList(criterioAvaliacaoList);
 
-        ListView listaEstandes = (ListView) view.findViewById(R.id.fragment_avaliar_estande_listaEstandes);
+        final ListView listaEstandes = (ListView) view.findViewById(R.id.fragment_avaliar_estande_listaEstandes);
 
         Call buscarCriteriosJuradoCall = new RetrofitInicializador().getCriterioJuradoService().getAll();
+        final ProgressDialog progressDialog = new ProgressDialog(getContext());
+        //progressDialog.setTitle("Carregando");
+        progressDialog.setMessage("Carregando... por favor aguarde.");
+        progressDialog.show();
 
         buscarCriteriosJuradoCall.enqueue(new Callback() {
+
             @Override
             public void onResponse(Call call, Response response) {
-                Object criterioJuradoList = response.body();
+                List<CriterioJurado> criterioJuradoList = (List<CriterioJurado>)response.body();
                 Toast.makeText(getContext(), criterioJuradoList.toString(), Toast.LENGTH_SHORT).show();
+                progressDialog.cancel();
+                progressDialog.dismiss();
+//                criterioJuradoList = usuario.getCriterioAvaliacaoList();
+                //Montar um mapa com chave sendo titulo do estande e valor a lista de criterios daquele estande.
+                final Map<String, List<CriterioJurado>> mapaDeCriterios = getMapEstandeFromListCriterio(criterioJuradoList);
+
+                //Preencher o listView com um array de strings sendo os titulos dos estandes.
+                List<String> strings = new ArrayList<>();
+                Object[] array = mapaDeCriterios.keySet().toArray();
+                for(int i = 0; i < array.length; i++){
+                    strings.add((String)array[i]);
+                }
+
+                ArrayAdapter<String> adapterEstandes = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, strings);
+                listaEstandes.setAdapter(adapterEstandes);
+
+                listaEstandes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                        //Quando o usuario clicar no estande, deve enviar para o fragment a lista de criteriosJurado para o fragment
+                        List<CriterioJurado> listaDeCriterios = mapaDeCriterios.get((String)adapterView.getItemAtPosition(position));
+                        CriterioAvalicaoFragment criterioAvalicaoFragment = new CriterioAvalicaoFragment();
+
+                        Bundle args = new Bundle();
+                        args.putSerializable("criterioList", (Serializable) listaDeCriterios);
+
+                        criterioAvalicaoFragment.setArguments(args);
+
+                        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.fragment_id, criterioAvalicaoFragment);
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                    }
+                });
             }
 
             @Override
@@ -109,46 +152,9 @@ public class AvaliarEstandeFragment extends Fragment {
             }
         });
 
-        List<CriterioJurado> criterioJuradoList = usuario.getCriterioAvaliacaoList();
-        //Montar um mapa com chave sendo titulo do estande e valor a lista de criterios daquele estande.
-        final Map<String, List<CriterioJurado>> mapaDeCriterios = new HashMap<>();
-        for(CriterioJurado criterio : criterioJuradoList){
-            String tituloEstande = criterio.getEstande().getTitulo();
-            if(mapaDeCriterios.get(tituloEstande) == null){
-                mapaDeCriterios.put(tituloEstande, new ArrayList<CriterioJurado>());
-            }
 
-            mapaDeCriterios.get(tituloEstande).add(criterio);
-        }
 
-        //Preencher o listView com um array de strings sendo os titulos dos estandes.
-        List<String> strings = new ArrayList<>();
-        Object[] array = mapaDeCriterios.keySet().toArray();
-        for(int i = 0; i < array.length; i++){
-            strings.add((String)array[i]);
-        }
 
-        ArrayAdapter<String> adapterEstandes = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, strings);
-        listaEstandes.setAdapter(adapterEstandes);
-
-        listaEstandes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                //Quando o usuario clicar no estande, deve enviar para o fragment a lista de criteriosJurado para o fragment
-                List<CriterioJurado> listaDeCriterios = mapaDeCriterios.get((String)adapterView.getItemAtPosition(position));
-                CriterioAvalicaoFragment criterioAvalicaoFragment = new CriterioAvalicaoFragment();
-
-                Bundle args = new Bundle();
-                args.putSerializable("criterioList", (Serializable) listaDeCriterios);
-
-                criterioAvalicaoFragment.setArguments(args);
-
-                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                fragmentTransaction.replace(R.id.fragment_id, criterioAvalicaoFragment);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
-            }
-        });
 
 //        botaoAvaliar = (Button) view.findViewById(R.id.avaliar_id);
 //
@@ -166,6 +172,20 @@ public class AvaliarEstandeFragment extends Fragment {
 //        });
 
         return view;
+    }
+
+    @NonNull
+    private Map<String, List<CriterioJurado>> getMapEstandeFromListCriterio(List<CriterioJurado> criterioJuradoList) {
+        final Map<String, List<CriterioJurado>> mapaDeCriterios = new HashMap<>();
+        for(CriterioJurado criterio : criterioJuradoList){
+            String tituloEstande = criterio.getEstande().getTitulo();
+            if(mapaDeCriterios.get(tituloEstande) == null){
+                mapaDeCriterios.put(tituloEstande, new ArrayList<CriterioJurado>());
+            }
+
+            mapaDeCriterios.get(tituloEstande).add(criterio);
+        }
+        return mapaDeCriterios;
     }
 
 }
