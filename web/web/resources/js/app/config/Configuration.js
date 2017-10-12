a91be27900c36d1c1b103d74300ed367a87aba73
Marcos Antonio dos Santos <marcaosi2014@gmail.com>
2017-10-12 11:38:33 -0300

    Configuracoes realizadas

--- web/web/resources/js/app/config/Configuration.js
+++ web/web/resources/js/app/config/Configuration.js
@@ -6,6 +6,6 @@ class Configuration{
 
     static getUrlWebApp(){
         // return 'http://18.220.190.201:8080/web/'
-        return 'http://localhost:8080/localizae/'
+        return 'http://localhost:8080/localizae'
     }
 }
