1e83dd34d8dbb3dd9b7ccc250742b5ba2fc15152
Marcos Antonio dos Santos <marcaosi2014@gmail.com>
2017-10-13 11:13:08 -0300

    Atualização do repositorio

--- mobile/LocalizaE/app/src/main/java/localizae/net/br/retrofit/RetrofitInicializador.java
+++ mobile/LocalizaE/app/src/main/java/localizae/net/br/retrofit/RetrofitInicializador.java
@@ -1,38 +1,22 @@
-package localizae.net.br.retrofit;
+package localizae.net.br.Retrofit;
 
-import localizae.net.br.model.AvaliacaoJurado;
-import localizae.net.br.services.endpoints.AvaliacaoJuradoInterface;
-import localizae.net.br.services.endpoints.CriterioJuradoInterface;
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
+import localizae.net.br.services.endpoints.BoothEndpointInterface;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
- * Created by marca on 10/10/2017.
+ * Created by Everton on 13/10/2017.
  */
 
 public class RetrofitInicializador {
-
     private final Retrofit retrofit;
 
-    public RetrofitInicializador() {
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+    public RetrofitInicializador (){
+        retrofit = new Retrofit.Builder().baseUrl("http://18.220.190.201:8080/localizae/").addConverterFactory(GsonConverterFactory.create()).build();
 
-        OkHttpClient.Builder client = new OkHttpClient.Builder();
-        client.addInterceptor(interceptor);
-
-        retrofit = new Retrofit.Builder()
-                .baseUrl("http://18.220.190.201:8080/localizae/")
-                .addConverterFactory(GsonConverterFactory.create()).build();
     }
 
-    public AvaliacaoJuradoInterface getAvaliacaoJuradoService(){
-        return retrofit.create(AvaliacaoJuradoInterface.class);
-    }
-
-    public CriterioJuradoInterface getCriterioJuradoService(){
-        return retrofit.create(CriterioJuradoInterface.class);
+    public BoothEndpointInterface getAvaliacaoVisitanteService(){
+        return retrofit.create(BoothEndpointInterface.class);
     }
 }
