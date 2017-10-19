301119bca26dfa1edd334566175c022836cfa30e
Marcos Antonio dos Santos <marcaosi2014@gmail.com>
2017-10-19 13:41:56 -0200

    resolve validate of user

--- web/web/resources/js/app/service/UsuarioService.js
+++ web/web/resources/js/app/service/UsuarioService.js
@@ -81,16 +81,16 @@ class UsuarioService {
     }
 
     validate(user) {
-        if (user.nome == null) {
-            throw new Error('Campo Nome obrigatório')
+        if (user.nome == "") {
+            throw 'Campo Nome obrigatório'
         }
 
-        if (user.email == null) {
-            throw new Error('Campo Email obrigatório')
+        if (user.email == "") {
+            throw 'Campo Email obrigatório'
         }
 
-        if (user.senha == null) {
-            throw new Error('Campo Senha obrigatório')
+        if (user.senha == "") {
+            throw 'Campo Senha obrigatório'
         }
     }
 }
